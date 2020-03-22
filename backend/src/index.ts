9b64e52d4c67937fc1bdb87d4a1fb7e9e30f7138
LoKi1237 <loki1237@yandex.ru>
2020-03-22 19:32:40 +0300

    Add env variables

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,28 +1,31 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import homeRoute from './routes/home';
-import userRoute from './routes/users';
-import messageRoute from './routes/messages';
-import noteRoute from './routes/notes';
-import bookmarkRoute from './routes/bookmarks';
+
 import { createConnection } from 'typeorm';
 import 'reflect-metadata';
+import dotenv from 'dotenv';
 
-const app = express();
-const port = 3001;
+import { authRouter } from './routes/auth';
+import { userRouter } from './routes/users';
+import { bookmarkRouter } from './routes/bookmarks';
+import { noteRouter } from './routes/notes';
+import { messageRouter } from './routes/messages';
+
+dotenv.config();
 
 createConnection().then(() => {
+    const app = express();
+    const port = 3001;
 
     app.use(bodyParser.json());
     app.use(cookieParser());
     app.use('/api/avatars', express.static("avatars"));
-
-    app.use('/api/', homeRoute);
-    app.use('/api/users', userRoute);
-    app.use('/api/messages', messageRoute);
-    app.use('/api/notes', noteRoute);
-    app.use('/api/bookmarks', bookmarkRoute);
+    app.use('/api/auth', authRouter());
+    app.use('/api/users', userRouter());
+    app.use('/api/bookmarks', bookmarkRouter());
+    app.use('/api/notes', noteRouter());
+    app.use('/api/messages', messageRouter());
 
     app.listen(port, () => console.log(`Server is runned on port ${port}`));
 
