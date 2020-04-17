af1b9e6c07e844e8fa1787d4a3607ac20ee3b545
LoKi1237 <loki1237@yandex.ru>
2020-04-17 22:16:17 +0300

    Added editing methods

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -19,7 +19,7 @@ const signUp = async (req: Request, res: Response) => {
 
     try {
         const validator = new DataValidation();
-        await validator.validate(userData);
+        await validator.validateAll(userData);
 
         const passwordHash = await bcrypt.hash(userData.password, 12);
         const newUser = await userRepository.create({
@@ -32,7 +32,11 @@ const signUp = async (req: Request, res: Response) => {
             city: userData.city,
             family_status: "not selected",
             avatar: "default.png",
-            status: "offline"
+            status: "offline",
+            activity: "",
+            interests: "",
+            hobby: "",
+            about_self: ""
         });
         await userRepository.save(newUser);
 
@@ -64,18 +68,18 @@ const login = async (req: Request, res: Response) => {
         if (!user) {
             throw new Error();
         } else {
-            const correctPassword = await bcrypt.compare(password, user.password);
+            const validPassword = await bcrypt.compare(password, user.password);
 
-            if (!correctPassword) {
+            if (!validPassword) {
                 throw new Error();
             }
         }
 
         const JWT = await jwt.sign({ email: user.email }, process.env.JWT_PRIVATE_KEY);
 
-        return res.status(200).header('Set-cookie', `JWT=${JWT}; max-age=2592000; httpOnly`).send();
+        return res.status(200).cookie("JWT", JWT, { maxAge: 2592000, httpOnly: true }).send();
     } catch {
-        return res.status(401).send("Incorrect login or password");
+        return res.status(401).send("Invalid login or password");
     }
 }
 
@@ -99,7 +103,7 @@ const loginAs = async (req: Request, res: Response) => {
 
         if (user) {
             return res.status(200)
-                      .header('Set-cookie', `JWT=${JWT}; max-age=2592000; httpOnly`)
+                      .cookie("JWT", JWT, { maxAge: 2592000, httpOnly: true })
                       .json({ id: user.id }).send();
         } else {
             return res.sendStatus(401);
@@ -110,7 +114,66 @@ const loginAs = async (req: Request, res: Response) => {
 }
 
 const logout = (req: Request, res: Response) => {
-    return res.status(200).header('Set-cookie', `JWT=0; max-age=0`).send();
+    return res.clearCookie('JWT').send();
+}
+
+const changeEmail = async (req: Request, res: Response) => {
+    if (!process.env.JWT_PRIVATE_KEY) {
+        console.log('Error: not found JWT_PRIVATE_KEY');
+        return res.status(500);
+    }
+
+    const userRepository = getRepository(Users);
+    const user = await userRepository.findOne({ id: +req.params.id });
+    const newEmail = req.body.newEmail;
+
+    if (!user) {
+        return res.status(400).send();
+    }
+
+    try {
+        const validator = new DataValidation();
+        await validator.validateEmail(newEmail);
+
+        userRepository.merge(user, { email: newEmail });
+        await userRepository.save(user);
+
+        const JWT = await jwt.sign({ email: newEmail }, process.env.JWT_PRIVATE_KEY);
+
+        return res.status(200).cookie("JWT", JWT, { maxAge: 2592000, httpOnly: true }).send();
+    } catch (err) {
+        return res.status(400).json({ error:  err.message });
+    }
+}
+
+const changePassword = async (req: Request, res: Response) => {
+    const userRepository = getRepository(Users);
+    const user = await userRepository.findOne({ id: +req.params.id });
+    const { oldPassword, newPassword } = req.body;
+
+    if (!user) {
+        return res.status(400).send();
+    }
+
+    try {
+        const validPassword = await bcrypt.compare(oldPassword, user.password);
+
+        if (!validPassword) {
+            throw new Error("Invalid password");
+        }
+
+        const validator = new DataValidation();
+        await validator.validatePassword(newPassword);
+
+        const passwordHash = await bcrypt.hash(newPassword, 12);
+
+        userRepository.merge(user, { password: passwordHash });
+        await userRepository.save(user);
+
+        return res.sendStatus(200).end();
+    } catch (err) {
+        return res.status(400).json({ error:  err.message });
+    }    
 }
 
 export function authRouter() {
@@ -120,6 +183,8 @@ export function authRouter() {
     router.post('/login-as', loginAs);
     router.post('/sign-up', signUp);
     router.head('/logout', logout);
+    router.put('/change_email/:id', changeEmail);
+    router.put('/change_password/:id', changePassword);
 
     return router;
 }
