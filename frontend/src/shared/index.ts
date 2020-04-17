f8833f08297243b0b789cf52be3e6a4310c7e395
LoKi1237 <loki1237@yandex.ru>
2020-04-17 22:20:01 +0300

    Made a convenient import

---
+++ frontend/src/shared/index.ts
@@ -0,0 +1,55 @@
+import Backdrop from './Backdrop/Backdrop';
+import Button from './Button/Button';
+import Checkbox from './Checkbox/Checkbox';
+import DatePicker from './DatePicker/DatePicker';
+import Divider from './Divider/Divider';
+import DropdownContainer from './Dropdown/DropdownContainer';
+import DropdownItem from './Dropdown/DropdownItem';
+import DropdownMenu from './Dropdown/DropdownMenu';
+import IconButton from './IconButton/IconButton';
+import InputField from './InputField/InputField';
+import Label from './Label/Label';
+import Loading from './Loading/Loading';
+import ModalBody from './Modal/ModalBody';
+import ModalFooter from './Modal/ModalFooter';
+import ModalHeader from './Modal/ModalHeader';
+import ModalImage from './Modal/ModalImage';
+import ModalWindow from './Modal/ModalWindow';
+import Radio from './Radio/Radio';
+import Row from './Row/Row';
+import Select from './Select/Select';
+import Slider from './Slider/Slider';
+import Spoiller from './Spoiller/Spoiller';
+import Stepper from './Stepper/Stepper';
+import Switch from './Switch/Switch';
+import TextArea from './TextArea/TextArea';
+import TopBar from './TopBar/TopBar';
+
+export {
+    Backdrop,
+    Button,
+    Checkbox,
+    DatePicker,
+    Divider,
+    DropdownContainer,
+    DropdownItem,
+    DropdownMenu,
+    IconButton,
+    InputField,
+    Label,
+    Loading,
+    ModalBody,
+    ModalFooter,
+    ModalHeader,
+    ModalImage,
+    ModalWindow,
+    Radio,
+    Row,
+    Select,
+    Slider,
+    Spoiller,
+    Stepper,
+    Switch,
+    TextArea,
+    TopBar
+}
