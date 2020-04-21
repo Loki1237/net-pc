6247c5232be2d3cf5d359a5576361773b84682ee
LoKi1237 <loki1237@yandex.ru>
2020-04-22 01:39:36 +0300

    Added index.ts

---
+++ backend/src/entity/index.ts
@@ -0,0 +1,15 @@
+import { Bookmarks } from './Bookmarks';
+import { Messages } from './Messages';
+import { Music } from './Music';
+import { Notes } from './Notes';
+import { Photo } from './Photo';
+import { Users } from './Users';
+
+export {
+    Bookmarks,
+    Messages,
+    Music,
+    Notes,
+    Photo,
+    Users
+}
