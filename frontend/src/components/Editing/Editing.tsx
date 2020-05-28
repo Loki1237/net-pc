946a1c8afa3b88d793090eaf79a9cfa4d5286863
LoKi1237 <loki1237@yandex.ru>
2020-05-28 19:04:17 +0300

    Used Tabs

--- frontend/src/components/Editing/Editing.tsx
+++ frontend/src/components/Editing/Editing.tsx
@@ -5,28 +5,34 @@ import AboutSelf from '../../containers/Editing/AboutSelf';
 import BasicData from '../../containers/Editing/BasicData';
 import Settings from '../../containers/Editing/Settings';
 
+import { Tabs, Tab } from '../../shared';
+
 interface Props {
     urlParam: string
 }
 
 const Editing = (props: Props) => {
-    switch (props.urlParam) {
-        case 'basic':
-            return <BasicData />
+    return (
+        <div className={styles.Editing}>
+            <Tabs>
+                <Tab href="/edit/basic" active={props.urlParam === "basic"}>
+                    Основное
+                </Tab>
 
-        case 'about_self':
-            return <AboutSelf />
+                <Tab href="/edit/about_self" active={props.urlParam === "about_self"}>
+                    О себе
+                </Tab>
 
-        case 'settings':
-            return <Settings />
+                <Tab href="/edit/settings" active={props.urlParam === "settings"}>
+                    Настройки
+                </Tab>
+            </Tabs>
 
-        default:
-            return (
-                <div className={styles.Settings}>
-                    <h1>Not found</h1>
-                </div>
-            );
-    }
+            {props.urlParam === "basic" && <BasicData />}
+            {props.urlParam === "about_self" && <AboutSelf />}
+            {props.urlParam === "settings" && <Settings />}
+        </div>
+    );
 }
 
 export default Editing;
