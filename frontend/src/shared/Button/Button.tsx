14a03483b5615a3bf11f94c17497426b33b88bea
LoKi1237 <loki1237@yandex.ru>
2020-06-11 13:27:20 +0300

    Added variant prop

--- frontend/src/shared/Button/Button.tsx
+++ frontend/src/shared/Button/Button.tsx
@@ -3,10 +3,11 @@ import styles from './Styles.m.css';
 import classNames from 'classnames';
 
 interface Props {
-    size?: string,
+    size?: "small" | "medium" | "large",
     disabled?: boolean,
     style?: object,
-    color?: string,
+    color?: "primary" | "secondary" | "info" | "success" | "warning" | "error",
+    variant?: "text" | "filled",
     children?: React.ReactNode,
     onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }
@@ -14,21 +15,22 @@ interface Props {
 const Button = (props: Props) => {
     const buttonClassNames = classNames({
         [styles.Button]: true,
-        [styles[`${props.size}`]]: props.size
+        [styles[`${props.size}`]]: props.size,
+        [styles.only_text]: props.variant === "text"
     });
+
+    const style = {
+        "--bg_color": props.variant !== "text" ? `var(--${props.color || "default"}-color)` : "transparent",
+        "--text_color": props.variant === "text" ? `var(--${props.color || "default"}-color)` : "#FFF"
+    } as React.CSSProperties;
 
     return (
         <button
             className={buttonClassNames}
-            style={!props.disabled 
-                ? {
-                    background: props.color ? `var(--${props.color}-color)` : "#777",
-                    ...props.style
-                } 
-                : { 
-                    ...props.style 
-                }
-            }
+            style={{
+                ...style,
+                ...props.style
+            }}
             disabled={props.disabled}
             onClick={props.onClick}
         >
